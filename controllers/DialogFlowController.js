1afa2fedba37cd8af6cebf6d1f59ce3799077693
Herisson Silva <herissonsilvahs@gmail.com>
2019-02-05 00:46:00 -0300

    adjust route textQuery response; implement eventQuery router

--- controllers/DialogFlowController.js
+++ controllers/DialogFlowController.js
@@ -2,12 +2,11 @@ const chatbotService = require('../services/ChatbotService')
 
 module.exports = {
   async text (req, res) {
-
-    const responses = chatbotService.textQuery(req.body.text, req.body.parameters)
-
+    const responses = await chatbotService.textQuery(req.body.text, req.body.parameters)
     res.status(200).json(responses)
   },
   async event (req, res) {
-
+    const responses = await chatbotService.eventQuery(req.body.event, req.body.parameters)
+    res.status(200).json(responses)
   }
 }
--- services/ChatbotService.js
+++ services/ChatbotService.js
@@ -3,6 +3,8 @@ const uuid = require('uuid')
 const sessionId = uuid.v4()
 const sessionClient = new dialogflow.SessionsClient()
 const sessionPath = sessionClient.sessionPath(process.env.googleProjectID, sessionId)
+const structjson = require('../helpers/structjson')
+const language = process.env.dialogflowSessionLanguageCode
 
 module.exports = {
   async textQuery (text, parameters = {}) {
@@ -13,11 +15,32 @@ module.exports = {
           // The query to send to the dialogflow agent
           text: text,
           // The language used by the client (en-US)
-          languageCode: 'en-US',
+          languageCode: language,
         },
       },
+      queryParams: {
+        payload: {
+          data: parameters
+        }
+      }
     }
     const responses = await sessionClient.detectIntent(request)
+    console.log(responses)
     return responses
+  },
+  async eventQuery (event, parameters = {}) {
+    const request = {
+      session: sessionPath,
+      queryInput: {
+        event: {
+          // The query to send to the dialogflow agent
+          name: event,
+          parameters: structjson.jsonToStructProto(parameters),
+          // The language used by the client (en-US)
+          languageCode: language,
+        },
+      }
+    }
+    return await sessionClient.detectIntent(request)
   }
 }
