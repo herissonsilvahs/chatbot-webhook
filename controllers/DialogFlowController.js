78e890249404bab58897b66c7094a273ec8c3324
Herisson Silva <herissonsilvahs@gmail.com>
2019-02-11 23:12:01 -0300

    funcional version finished

--- controllers/DialogFlowController.js
+++ controllers/DialogFlowController.js
@@ -2,11 +2,24 @@ const chatbotService = require('../services/ChatbotService')
 
 module.exports = {
   async text (req, res) {
-    const responses = await chatbotService.textQuery(req.body.text, req.body.parameters)
-    res.status(200).json(responses)
+    try {
+      const responses = await chatbotService.textQuery(req.body.text, req.body.parameters)
+      res.status(200).json(responses)
+    } catch (err) {
+      const objError = {error: {errorName: err.name, errorMessage: err.message}}
+      res.status(500).json(objError)
+    }
   },
   async event (req, res) {
     const responses = await chatbotService.eventQuery(req.body.event, req.body.parameters)
     res.status(200).json(responses)
+
+    try {
+      const responses = await chatbotService.eventQuery(req.body.event, req.body.parameters)
+      res.status(200).json(responses)
+    } catch (err) {
+      const objError = {error: {errorName: err.name, errorMessage: err.message}}
+      res.status(500).json(objError)
+    }
   }
 }
--- services/ChatbotService.js
+++ services/ChatbotService.js
@@ -4,43 +4,49 @@ const sessionId = uuid.v4()
 const sessionClient = new dialogflow.SessionsClient()
 const sessionPath = sessionClient.sessionPath(process.env.googleProjectID, sessionId)
 const structjson = require('../helpers/structjson')
-const language = process.env.dialogflowSessionLanguageCode
+const language = process.env.dialogFlowSessionLanguageCode
 
 module.exports = {
   async textQuery (text, parameters = {}) {
-    const request = {
-      session: sessionPath,
-      queryInput: {
-        text: {
-          // The query to send to the dialogflow agent
-          text: text,
-          // The language used by the client (en-US)
-          languageCode: language,
+    try {
+      const request = {
+        session: sessionPath,
+        queryInput: {
+          text: {
+            // The query to send to the dialogflow agent
+            text: text,
+            // The language used by the client (en-US)
+            languageCode: language,
+          },
         },
-      },
-      queryParams: {
-        payload: {
-          data: parameters
+        queryParams: {
+          payload: {
+            data: parameters
+          }
         }
       }
+      return await sessionClient.detectIntent(request)
+    } catch (err) {
+      throw new Error(err)
     }
-    const responses = await sessionClient.detectIntent(request)
-    console.log(responses)
-    return responses
   },
   async eventQuery (event, parameters = {}) {
-    const request = {
-      session: sessionPath,
-      queryInput: {
-        event: {
-          // The query to send to the dialogflow agent
-          name: event,
-          parameters: structjson.jsonToStructProto(parameters),
-          // The language used by the client (en-US)
-          languageCode: language,
-        },
+    try {
+      const request = {
+        session: sessionPath,
+        queryInput: {
+          event: {
+            // The query to send to the dialogflow agent
+            name: event,
+            parameters: structjson.jsonToStructProto(parameters),
+            // The language used by the client (en-US)
+            languageCode: language,
+          },
+        }
       }
+      return await sessionClient.detectIntent(request)
+    } catch (err) {
+      throw new Error(err)
     }
-    return await sessionClient.detectIntent(request)
   }
 }
