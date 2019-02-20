22181c77527492b0e6811acf46a67580e6499065
Herisson Silva <herissonsilvahs@gmail.com>
2019-02-20 09:51:30 -0300

    update webhook router

--- controllers/DialogFlowWebhookController.js
+++ controllers/DialogFlowWebhookController.js
@@ -1,6 +1,17 @@
+const currencyConvert = require('../helpers/currencyConvvert')
+
 module.exports = {
   async start (req, res) {
-    console.log(req)
-    return res.status(200).json(req)
+    try {
+      const { queryResult } = req.body
+      if (queryResult.action === 'currency.convert') {
+        const valueConverted = await currencyConvert(queryResult.parameters)
+        return res.status(200).json({message: `O Valor convertido é ${valueConverted}`})
+      }
+      return res.status(200).json({message: "Desculpa não consegui converter, tente novamente!"})
+    } catch (err) {
+      const objError = {error: {errorName: err.name, errorMessage: err.message}}
+      res.status(500).json(objError)
+    }
   }
 }
