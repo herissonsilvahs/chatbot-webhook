f640fdec98afc3937cb7c411c3b95b142748663b
Herisson Silva <herissonsilvahs@gmail.com>
2019-02-20 10:19:11 -0300

    ajustes de error, mostrar console log

--- helpers/currencyConvvert.js
+++ helpers/currencyConvvert.js
@@ -1,16 +1,22 @@
 const CurrencyApiService = require('../services/CurrencyApiService')
 
 module.exports = async (parameters) => {
-  if (!parameters) return new Error("Parameters not be null")
-  const currencyTo = parameters['currency-to'].stringValue
-  const currencyFrom = parameters['currency-from'].stringValue
-  const amount = parameters['amount'].numberValue
+  try {
+    if (!parameters) return new Error("Parameters not be null")
+    const currencyTo = parameters['currency-to'].stringValue
+    const currencyFrom = parameters['currency-from'].stringValue
+    const amount = parameters['amount'].numberValue
 
-  if (!amount || !currencyFrom || !currencyTo) return new Error("Missing params")
+    console.log(currencyFrom, currencyTo, amount)
 
-  const currencies = (await CurrencyApiService.get(`latest?base=${currencyFrom}`)).data
-  const { date, rates } = currencies
-  let convertedResult = (rates[currencyTo] * amount).toLocaleString(currencyTo, { style: 'currency', currency: currencyTo })
+    if (!amount || !currencyFrom || !currencyTo) throw new Error("Missing params")
 
-  return convertedResult
+    const currencies = (await CurrencyApiService.get(`latest?base=${currencyFrom}`)).data
+    const { date, rates } = currencies
+    let convertedResult = (rates[currencyTo] * amount).toLocaleString(currencyTo, { style: 'currency', currency: currencyTo })
+
+    return convertedResult
+  } catch (err) {
+    throw new Error(err)
+  }
 }
