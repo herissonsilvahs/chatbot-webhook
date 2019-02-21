63bca287d6f30d961cdd0f0cabd2fdede85f6289
Herisson Silva <herissonsilvahs@gmail.com>
2019-02-21 14:13:15 -0300

    fix

--- controllers/DialogFlowWebhookController.js
+++ controllers/DialogFlowWebhookController.js
@@ -20,44 +20,12 @@ module.exports = {
                   }
                 }
               ]
-            },
-
-            "systemIntent": {
-              "intent": "actions.intent.OPTION",
-              "data": {
-                "@type": "type.googleapis.com/google.actions.v2.OptionValueSpec",
-                "listSelect": {
-                  "title": "Açoes",
-                  "items": [
-                    {
-                      "optionInfo": {
-                        "key": "SELECTION_KEY_CONVERCAO",
-                        "synonyms": [
-                          "Converter"
-                        ]
-                      },
-                      "title": "Converter moedas"
-                    },
-                    {
-                      "optionInfo": {
-                        "key": "SELECTION_KEY_COTACAO",
-                        "synonyms": [
-                          "Cotação do dolar",
-                          "Valor do dolar"
-                        ]
-                      },
-                      "title": "Cotação"
-                    }
-                  ]
-                }
-              }
             }
           }
         }
       })
     } catch (err) {
-      // const objError = {error: {errorName: err.name, errorMessage: err.message}}
-      res.status(500).json({
+      res.status(200).json({
         "fulfillmentText": "Desculpa não entendi"
       })
     }
